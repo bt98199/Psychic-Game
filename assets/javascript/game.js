2cc071cb5eef70b9286a664cad381e669d1584fb
Bronson <bt98199@gmail.com>
2019-03-03 16:29:54 -0800

    First one done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
   // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-  var computerChoices = ["r", "p", "s"];
+  var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var guessHistoryArray = [];
 
   // Creating variables to hold the number of wins, losses, and ties. They start at 0.
   var wins = 0;
   var losses = 0;
-  var guesses = 10;
- 
+  var guesses = 9;
+  var letterToGuess = undefined;
+
 
   // Create variables that hold references to the places in the HTML where we want to display things.
   var directionsText = document.getElementById("directions-text");
@@ -17,48 +18,59 @@
   var guessesText = document.getElementById("guesses-left");
   var guessHistoryText = document.getElementById("guess-history");
 
-  // This function is run whenever the user presses a key.
+// Defining Functions
+//Build the array of past guesses
+
+
+function psyChoice() {
+    letterToGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(letterToGuess);
+};
+
+function reset() {
+  guesses = 9;
+  guessHistoryArray = [];
+  psyChoice();
+}
+
+  // Sets the Psychic choice. 
+  psyChoice();
+
+  //******* This function is run whenever the user presses a key*******body*****//
   document.onkeyup = function (event) {
 
-      // Determines which key was pressed.
-      var userGuess = event.key;
+  // Determines which key was pressed. It is modified to convert all entries to lower case
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-      // Randomly chooses a choice from the options array. This is the Computer's guess.
-      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+  // Build initial history array
+  guessHistoryArray.push(userGuess);
+//  guessHistoryBuild();
+  
+  // 3 possible out comes, a win, a loss of guess, or a loss
+  if (guesses === 1) {
+          losses++;
+          document.querySelector('#losses-text').innerHTML = "Losses: " + losses;
+          alert("You lost, Carnac");
+          reset();
+        } else if (userGuess === letterToGuess) {
+          wins++;
+          document.querySelector('#wins-text').innerHTML = "Wins: " + wins;
+          alert("You got it, Kreskin!");
+          reset();
+        } else {
+        guesses--;
+     }
 
-      // Reworked our code from last step to use "else if" instead of lots of if statements.
-      guessHistoryArray.push(userGuess);
-      function guessHistory() {
-          guessHistoryArray.forEach(element => {
-              console.log(element + ", ");
-          });
-      }
-      // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-      if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
-
-          if ((userGuess === "r" && computerGuess === "s") ||
-              (userGuess === "s" && computerGuess === "p") ||
-              (userGuess === "p" && computerGuess === "r")) {
-              wins++;
-          } else if (userGuess === computerGuess) {
-              guesses--;
-          } else {
-              losses++;
-          }
-
-          // Hide the directions
-          // directionsText.textContent = "";
+// Hide the directions
+// directionsText.textContent = "";
 
           // Display the user and computer guesses, and wins/losses/ties.
           userChoiceText.textContent = "You chose: " + userGuess;
-          computerChoiceText.textContent = "The computer chose: " + computerGuess;
+          computerChoiceText.textContent = "The computer chose: " + letterToGuess;
           winsText.textContent = "Wins: " + wins;
           lossesText.textContent = "Losses: " + losses;
           guessesText.textContent = "Guesses Left: " + guesses;
-          guessHistoryText.textContent = guessHistory();
-          console.log("hello");
+          document.getElementById('guess-history').innerHTML = "Your Guesses so far: " + (guessHistoryArray.join(", "));
 
+}
 
-
-      }
-  };
